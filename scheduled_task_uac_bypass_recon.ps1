164aad4844ea931184ec372bbf97c39069e09ac5
alvarosanz1993 <63511997+alvarosanz1993@users.noreply.github.com>
2022-01-18 14:30:44 +0100

    Update scheduled_task_uac_bypass_recon.ps1

--- scheduled_task_uac_bypass_recon.ps1
+++ scheduled_task_uac_bypass_recon.ps1
@@ -1,8 +1,14 @@
+echo "Identificando tareas programadas debiles a una inyeccion de variables de entorno..."
 $task = Get-ScheduledTask |
   Where-Object { $_.State -ne "Disabled" -and
                  $_.Principal.Runlevel -ne "Limited" -and
                  $_.Principal.LogonType -ne "ServiceAccount" -and
                  $_.Actions[0].CimClass.CimClassName -eq "MSFT_TaskExecAction" -and
-                 $_.Actions[0].Execute -match '^%.+%.+$' }
+                 $_.Actions[0].Execute -match '^%.+%.+?$' }
 
 $task
+
+echo "Identificando el nombre de las variables de entorno debiles..."
+$pattern = '(?<=\%).+?(?=\%)'
+$testText = $task | ForEach-Object {(Get-ScheduledTask $_.TaskName).Actions[0]} | ForEach-Object {[regex]::Matches($_.Execute, $pattern).Value}
+$testText
